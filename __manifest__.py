dff45793df4fa4ebc4cb3326cfc492aecc3ff7b5
michael josue ponce villalta <maikelponce@outlook.es>
2017-06-06 17:43:44 +0100

    Modificado el manifest

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Michael S.L",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
